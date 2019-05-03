0799995253d9f7b73a7578f4af978874ed50154f
naveenamadepally13 <47007814+naveenamadepally13@users.noreply.github.com>
2019-05-03 15:01:15 -0500

    Added download functionality

--- BusinessLayer/routes/uploadMongo.js
+++ BusinessLayer/routes/uploadMongo.js
@@ -8,59 +8,13 @@ var Grid = require('gridfs-stream');
 var path = require('path');
 const fs = require('fs');
 var Archiver = require('archiver');
-var metaDataModel = require('../models/modelsMetadata');
-
-var Busboy = require('busboy');
-
 
 router.post('/models', (req,res) => {
+  console.log(req.body);
 
-  var metaInfo = new metaDataModel(req.body.metaInfo);
-
-  var upload = new uploadFile(req.body);
-  var conn = mongoose.connection;
-  var gfs = Grid(conn.db, mongoose.mongo);
-  gfs.collection('uploadFiles'); // set the collection to look up into
-  var readStream = gfs.createReadStream({
-    _id: req.body.metaInfo.file_id
-  });
-  const chunks = [];
-  var str = '';
-  readStream.on("data", function (chunk) {
-    chunks.push(chunk);
-  });
-  // Send the buffer or you can put it into a var
-  readStream.on("end", function () {
-    str = Buffer.concat(chunks).toString();
-    let obj = {};
-    let splitted = str.split("\n");
-    for (let i = 0; i < splitted.length - 1; i++) {
-      let splitLine = splitted[i].split(":");
-      obj[splitLine[0]] = splitLine[1].replace(/(\r\n|\n|\r)/gm, "");
-    }
-    // loop form data as key value pairs and pushed to existing "obj"
-    // for (var entry of formData.entries()) {
-    //   obj[entry[0]]  = entry[1];
-    // }
-
-    obj.Author= metaInfo.Author;
-    obj.categoryID= metaInfo.categoryID;
-    obj.model_name= metaInfo.model_name;
-    obj.experiment= metaInfo.experiment;
-    metaDataModel.create(obj, function (err, data) {
-
-      upload.metaID = data._id;
-      uploadFile.create(upload, function (err, data) {
-
-        try {
-          metaObj.experimentID = data._id;
-          res.json(data);
-        } catch (error) {
-          res.json(error);
-        }
-      });
-
-    });
+  uploadFile.create(req.body, function (err, post) {
+    if (err) return console.log("Error in creating model : "+err);
+    res.json(post);
   });
 });
 
@@ -68,9 +22,11 @@ router.get('/models', function (req, res, next) {
   console.log('inside models')
   var name = req.query.name;
   var userID = req.query.userID;
+  var experiment = req.query.experiment;
 
-  console.log(name)
-  console.log(userID)
+  // console.log(name)
+  // console.log(userID)
+  console.log(experiment)
 
   if ((name != undefined && name != "" && name != null) && (userID != undefined && userID != "" && userID != null)){
     console.log('both name and userID are given')
@@ -79,12 +35,18 @@ router.get('/models', function (req, res, next) {
       console.log(post)
       res.json(post);
     });
-  } else if ((userID != undefined && userID != "" && userID != null)){
+  } else if ((userID != undefined && userID != "" && userID != null) && (experiment == undefined) ){
     console.log('userId is given')
     uploadFile.find({"userId": userID}, function (err,post){
       if (err) return next(err);
       console.log(post)
       res.json(post);
+    });
+  } else if((userID != undefined && userID != "" && userID != null) && (experiment != undefined && experiment != "" && experiment != null)) {
+    console.log('when experiment is given')
+    uploadFile.find({"userId": userID, "experiment": experiment}, function (err, data) {
+      if (err) return next(err);
+      res.json(data);
     });
   } else if((name == undefined ) && (userID == undefined)) {
     console.log('name and userId are not given')
@@ -104,54 +66,6 @@ router.get('/models/:name', function(req, res, next){
 });
 
 router.post('/files', (req, res) => {
-
-  try{
-  // var wholeData = '';
-  // //busboy is used to interpret streaming request
-  // var busboy = new Busboy({ headers: req.headers });
-  // let formData = new Map();
-  // //busboy to extract form data
-  // busboy.on('field', function(fieldname, val) {
-  //  // console.log("Entered bus boy")
-  //   formData.set(fieldname, val);
-  // });
-  // //busboy to extract file information
-  // busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
-
-  //   console.log(filename);
-  //   if(/_metadata.txt/.test(filename)){
-
-  //     console.log('File [' + fieldname + ']: filename: ' + filename);
-
-  //     file.on('data', function(data) {
-  //       wholeData+=data;
-  //     });
-  //     file.on('end', function() {
-  //       console.log(wholeData.toString());
-  //       let obj = {};
-  //       let splitted = wholeData.toString().split("\n");
-  //       for (let i = 0; i<splitted.length-1; i++) {
-  //         let splitLine = splitted[i].split(":");
-  //         obj[splitLine[0]] = splitLine[1].replace(/(\r\n|\n|\r)/gm,"");
-  //       }
-  //       // loop form data as key value pairs and pushed to existing "obj"
-  //       for (var entry of formData.entries()) {
-  //         obj[entry[0]]  = entry[1];
-  //       }
-  //       metaDataModel.create(obj, function (err, data) {
-  //       })
-  //       //console.log(obj);
-  //     });
-  //   }
-
-  // });
-  // // Listen for event when Busboy is finished parsing the form.
-  // busboy.on("finish", function() {
-  //   //console.log(formData) // Map { 'name' => 'hi', 'number' => '4' }
-
-  // });
-
-
   upload(req,res, (err) => {
     if(err){
       console.log("Error in uploading file : "+err);
@@ -159,14 +73,6 @@ router.post('/files', (req, res) => {
     }
     res.json({error_code:0, error_desc: null, file_uploaded: true, file_id: req.file.id});
   });
-  //req.pipe(busboy);
-}
-catch(err)
-{
-  res.json(err);
-}
-
-
 });
 
 router.get('/files/:fileReferenceID', function(req, res, next){
@@ -190,7 +96,8 @@ router.get('/files/:fileReferenceID', function(req, res, next){
   });
 });
 
-router.get('/chunks/:fileID', function(req, res, next){
+router.get('/chunks/:fileName', function(req, res, next){
+  console.log(req.params)
   console.log("inside routes for fileID")
   let filesData = [];
   let count = 0;
@@ -199,30 +106,28 @@ router.get('/chunks/:fileID', function(req, res, next){
   var gfs = Grid(conn.db, mongoose.mongo);
   gfs.collection('uploadFiles'); // set the collection to look up into
   console.log(req.params.fileID);
-  // gfs.files.findOne({ filename: "LSTM_011019-173749_architecture.jpg" }, (err, file) => {
-  //   // Check if the input is a valid image or not
-  //   if (!file || file.length === 0) {
-  //     return res.status(404).json({
-  //       err: 'No file exists'
-  //     });
-  //   }
-  //
-  //   // If the file exists then check whether it is an image
-  //   if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
-  //     // Read output to browser
-  //     const readstream = gfs.createReadStream(file.filename);
-  //     readstream.pipe(res);
-  //   } else {
-  //     res.status(404).json({
-  //       err: 'Not an image'
-  //     });
-  //   }
-  // });
+  gfs.files.findOne({ filename: req.params.fileName }, (err, file) => {
+    // Check if the input is a valid image or not
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: 'No file exists'
+      });
+    }
 
-  var readStream = gfs.createReadStream({
-    _id: req.params.fileID
-  }).pipe(res);
-
+    // If the file exists then check whether it is an image
+    // if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+    // Read output to browser
+    const readstream = gfs.createReadStream(file.filename);
+    console.log("file content ====================")
+    console.log(file);
+    res.set('Content-Type', file.contentType)
+    readstream.pipe(res);
+    // } else {
+    //   res.status(404).json({
+    //     err: 'Not an image'
+    //   });
+    // }
+  });
 });
 
 
@@ -283,11 +188,4 @@ router.get('/files', (req, res, next) => {
 });
 
 
-// function metaDataParser(fileid, metaInfo) {
-//
-// }
-
-
 module.exports = router;
-
-
